117a716f31803b65847e9de1086f4c58419887d8
Sverre Magnus Haakonsen <sverremh@ntnu.no>
2021-12-16 09:56:40 +0100

    Update latest version.

--- Classes/SH_Element.cs
+++ Classes/SH_Element.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Rhino.Geometry;
 namespace SimpleShapeGrammar.Classes
 {
     [Serializable] 
@@ -14,6 +14,8 @@ namespace SimpleShapeGrammar.Classes
         public int? ID { get; set; }
         public string elementName { get; set; }
         public SH_Node[] Nodes { get; }
+
+        public NurbsCurve NurbsCurve { get; set; }
 
         public SH_CrossSection_Beam CrossSection { get; set; }
         
@@ -28,16 +30,21 @@ namespace SimpleShapeGrammar.Classes
             
             ID = _id;
             Nodes = _nodes;
+            CreateNurbs();
         }
         public SH_Element(SH_Node[] _nodes, int? _id, string _el_name)
         {
             ID = _id;
             Nodes = _nodes;
             elementName = _el_name;
+            CreateNurbs();
         }
 
 
         // --- methods ---
-
+        private void CreateNurbs()
+        {
+            NurbsCurve = NurbsCurve.Create(false, 1, new Point3d[]{Nodes[0].Position, Nodes[1].Position});
+        }
     }
 }
