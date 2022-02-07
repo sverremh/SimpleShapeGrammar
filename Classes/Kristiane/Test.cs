6c58121dddc79c32935b110ee6f10f9db74d9713
Kristiane Snarvold Sletten <kristssl@ntnu.no>
2022-02-07 14:48:08 +0100

    Ny classe

---
+++ Classes/Kristiane/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleShapeGrammar.Classes.Kristiane
+{
+    class Test
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public string Name { get; set; }
+        public Test()
+        {
+
+        }
+    }
+}
+
